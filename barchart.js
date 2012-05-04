ed18a96fef9b7f90365e5b0fd82d071299166620
Alexander Grüneberg <alexander.grueneberg@googlemail.com>
2012-05-03 23:09:42 -0500

    Rewrite barchart.

--- barchart.js
+++ barchart.js
@@ -1,104 +1,130 @@
 (function (exports) {
     "use strict";
 
+    /*jshint curly:false */
     /*global d3 */
 
- // Draws a barchart.
- // @param {object} data
- // @param {object} options
- // @param {DOMElement} options.parentElement The parent element of the SVG element.
- // @param {number} options.width Width of the SVG element.
- // @param {number} options.height Height of the SVG element.
- // @param {object} options.axes
- // @param {object} options.axes.y
- // @param {number} options.axes.y.padding
- // @param {number} options.axes.y.ticks
-    exports.barchart = function (data, options) {
+    exports.barchart = function () {
 
-        var labels, values, parentElement, width, height, yAxisPadding, yAxisTicks,
-            yMin, yMax, xScale, yScale, yAxisScale, yAxis, svg;
+        var barchart, width, height, yAxisPadding, yAxisTicks,
+            xScale, yScale, yInverseScale, xAxis, yAxis;
 
-     // Prepare input data.
-        labels = Object.keys(data);
-        values = [];
-        labels.forEach(function (label) {
-            values.push(data[label]);
-        });
+        width = 300;
+        height = 300;
 
-     // Evaluate options and set defaults.
-        options = options || {};
-        parentElement = options.parentElement || document.body;
-        width = options.width || 300;
-        height = options.height || 300;
-        if (options.hasOwnProperty("axes") && options.axes.hasOwnProperty("y") && options.axes.y.padding) {
-            yAxisPadding = options.axes.y.padding;
-        } else {
-            yAxisPadding = 25;
-        }
-        if (options.hasOwnProperty("axes") && options.axes.hasOwnProperty("y") && options.axes.y.ticks) {
-            yAxisTicks = options.axes.y.ticks;
-        } else {
-            yAxisTicks = height / 75;
-        }
+        xScale = d3.scale.ordinal();
 
-     // Create an ordinal scale for the x axis.
-        xScale = d3.scale.ordinal()
-                   .domain(labels)
-                   .rangeBands([yAxisPadding, width]);
+        yAxisPadding = 25;
+        yAxisTicks = height / 75;
+        yScale = d3.scale.linear();
+        yInverseScale = d3.scale.linear();
 
-     // Create a linear scale for the y axis.
-        yMin = d3.min(values);
-        yMax = d3.max(values);
-        yScale = d3.scale.linear()
-                   .domain([yMin, yMax])
-                   .range([0, height]);
-        yAxisScale = d3.scale.linear()
-                             .domain([yMin, yMax])
+        yAxis = d3.svg.axis()
+                  .scale(yInverseScale)
+                  .orient("left")
+                  .ticks(yAxisTicks)
+                  .tickSize(6, 0);
+
+        barchart = function (selection) {
+
+            selection.each(function (data) {
+
+                var yMin, yMax, svg, enter, group;
+
+             // Convert object to array.
+                data = d3.keys(data).map(function (d) {
+                    return [d, data[d]];
+                });
+
+             // Update x scale.
+                xScale.domain(d3.keys(data))
+                      .rangeBands([yAxisPadding, width]);
+
+             // Update padded y scale.
+                yMin = d3.min(data, function (d) {
+                    return d[1];
+                });
+                yMax = d3.max(data, function (d) {
+                    return d[1];
+                });
+                yScale.domain([yMin, yMax])
+                      .range([0, height]);
+                yInverseScale.domain([yMin, yMax])
                              .range([height, 0]);
 
-     // Create y axis.
-        yAxis = d3.svg.axis()
-                      .scale(yAxisScale)
-                      .orient("left")
-                      .ticks(yAxisTicks)
-                      .tickSize(5, 0);
+             // Select existing SVG elements.
+                svg = d3.select(this)
+                        .selectAll("svg")
+                        .data([data]) // Trick to create only one svg element for each data set.
 
-     // Set up the SVG element.
-        svg = d3.select(parentElement)
-                .append("svg")
-                .attr("width", width)
-                .attr("height", height);
+             // Create non-existing SVG elements.
+                svg.enter()
+                   .append("svg");
 
-     // Generate y axis.
-        svg.append("g")
-           .attr("class", "axis")
-         // Add penalty of 1 so that axis and first bar do not collide.
-           .attr("transform", "translate(" + (yAxisPadding - 1) + ",0)")
-           .call(yAxis);
+             // Update both existing and newly created SVG elements.
+                svg.attr("width", width)
+                   .attr("height", height);
 
-     // Generate and append bars.
-        svg.append("g")
-           .selectAll("rect")
-           .data(values)
-           .enter()
-           .append("rect")
-           .attr("class", "bar")
-           .attr("width", function (d, i) {
-                return xScale.rangeBand();
-            })
-           .attr("height", function (d) {
-                return yScale(d);
-            })
-           .attr("x", function (d, i) {
-                return yAxisPadding + (xScale.rangeBand() * i);
-            })
-           .attr("y", function (d) {
-               return height - yScale(d);
-            })
-           .append("title")
-           .text(function (d, i) {
-                return labels[i];
+             // Generate y axis.
+                svg.append("g")
+                   .attr("class", "axis")
+                 // Add penalty of 1 so that axis and first bar do not collide.
+                   .attr("transform", "translate(" + (yAxisPadding - 1) + ",0)")
+                   .call(yAxis);
+
+             // Generate bars.
+                svg.append("g")
+                   .selectAll("rect")
+                   .data(data)
+                   .enter()
+                   .append("rect")
+                   .attr("class", "bar")
+                   .attr("width", function (d) {
+                        return xScale.rangeBand();
+                    })
+                   .attr("height", function (d) {
+                        return yScale(d[1]);
+                    })
+                   .attr("x", function (d, i) {
+                        return yAxisPadding + (xScale.rangeBand() * i);
+                    })
+                   .attr("y", function (d) {
+                       return height - yScale(d[1]);
+                    })
+                   .append("title")
+                   .text(function (d) {
+                        return d[0];
+                    });
+
             });
+
+        };
+
+        barchart.height = function (_) {
+            if (!arguments.length) return height;
+            height = _;
+            return barchart;
+        };
+
+        barchart.width = function (_) {
+            if (!arguments.length) return width;
+            width = _;
+            return barchart;
+        };
+
+        barchart.yAxisPadding = function (_) {
+            if (!arguments.length) return yAxisPadding;
+            yAxisPadding = _;
+            return barchart;
+        };
+
+        barchart.yAxisTicks = function (_) {
+            if (!arguments.length) return yAxisTicks;
+            yAxisTicks = _;
+            return barchart;
+        };
+
+        return barchart;
 
     };
 
